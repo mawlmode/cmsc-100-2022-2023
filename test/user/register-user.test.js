61c19a238404ed2a3e28565348bf54e23fa1713c
Marl Gabriel Mendoza Decuzar <mmdecuzar@up.edu.ph>
2022-11-08 14:08:34 +0000

    feat: add register user

---
+++ test/user/register-user.test.js
@@ -0,0 +1,59 @@
+import tap from 'tap';
+import { build } from '../../src/app.js';
+import 'must/register.js';
+import Chance from 'chance';
+
+const chance = new Chance();
+
+tap.mochaGlobals();
+
+const prefix = '/api';
+
+describe('Register a user should work', async () => {
+  let app;
+
+  before(async () => {
+    app = await build();
+  });
+
+  const newUser = {
+    username: chance.email({ domain: 'example.com' }),
+    password: chance.string({ length: 12 }),
+    firstName: chance.first(),
+    lastName: chance.last()
+  };
+
+  it('Should return the user that was created with a new user', async () => {
+    const response = await app.inject({
+      method: 'POST',
+      url: `${prefix}/register`,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    });
+
+    response.statusCode.must.be.equal(200);
+
+    const result = await response.json();
+
+    result.username.must.be.equal(newUser.username);
+    result.firstName.must.be.equal(newUser.firstName);
+    result.lastName.must.be.equal(newUser.lastName);
+    result.createdDate.must.not.be.null();
+    result.updatedDate.must.not.be.null();
+  });
+
+  it('Should return error HTTP code 400 if using the same username', async () => {
+    const response = await app.inject({
+      method: 'POST',
+      url: `${prefix}/register`,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newUser)
+    });
+
+    response.statusCode.must.be.equal(400);
+  });
+});
