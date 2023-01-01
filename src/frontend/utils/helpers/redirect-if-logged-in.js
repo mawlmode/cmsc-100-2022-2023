770cc8e06c93e685c695ece2cc170772995f0efa
Marl Gabriel Mendoza Decuzar <mmdecuzar@up.edu.ph>
2023-01-01 10:46:05 +0000

    feat: add helpers and lit with no shadows

---
+++ src/frontend/utils/helpers/redirect-if-logged-in.js
@@ -0,0 +1,15 @@
+import { changeUrl } from './change-url.js';
+import { state } from '../../worker/index.js';
+
+export async function redirectIfLoggedIn () {
+    if (
+        userIsLoggedIn ||
+        //will check backend if session exists
+        (await window.fetch('/api/auth-check')).status === 200) {
+            //go to todos
+            await state.set('user-is-logged-in', true);
+            return changeUrl('/todos');
+        }
+
+        await state.set('user-is-logged-in', false);
+}
