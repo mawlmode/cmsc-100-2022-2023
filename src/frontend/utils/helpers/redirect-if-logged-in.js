f6ed5a4f0db2320651967b1a39c57a5a2985d82b
Marl Gabriel Mendoza Decuzar <mmdecuzar@up.edu.ph>
2023-01-07 17:28:43 +0000

    fix: 'user-is-logged-in' was left undefined

--- src/frontend/utils/helpers/redirect-if-logged-in.js
+++ src/frontend/utils/helpers/redirect-if-logged-in.js
@@ -2,6 +2,7 @@ import { changeUrl } from './change-url.js';
 import { state } from '../../worker/index.js';
 
 export async function redirectIfLoggedIn () {
+  const userIsLoggedIn = await state.get('user-is-logged-in');
   if (
     userIsLoggedIn ||
         // will check backend if session exists
