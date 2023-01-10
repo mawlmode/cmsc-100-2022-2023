e8ae2e3fc432eee13a496c9d4b50c9e73dbdcf57
Marl Gabriel Mendoza Decuzar <mmdecuzar@up.edu.ph>
2023-01-10 09:50:12 +0000

    fix: path to log-in properly referenced

--- src/frontend/pages/page-home/template.js
+++ src/frontend/pages/page-home/template.js
@@ -1,5 +1,5 @@
 import { html } from 'lit';
 
 export function template () {
-  return html`Welcome. <a href="\login">Please log in here.</a>`;
+  return html`Welcome. <a href="/login">Please log in here.</a>`;
 }
