02f337b28c38086af35813aed3962f5173ada0db
Marl Gabriel Mendoza Decuzar <mmdecuzar@up.edu.ph>
2023-01-10 02:37:57 +0000

    fix: typographical error in import

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -2,7 +2,7 @@ import '@tjmonsi/small-router/small-router.js';
 import { routes } from './routes.js';
 
 // loads the header-component to be rendered
-import './components/header-components/index.js';
+import './components/header-component/index.js';
 
 // get the router from body to initialize routes
 const router = document.querySelector('small-router');
